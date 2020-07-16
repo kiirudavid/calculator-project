8ea4425b9521274165a80d6e79e60cc5fd890b37
davidmaina085@gmail.com <davidmaina085@gmail.com>
2020-07-16 12:07:12 +0300

    added html

---
+++ js/scripts.js
@@ -0,0 +1,34 @@
+var add = function(number1, number2) {
+    return number1 + number2;
+    };
+    var number1= parseInt(prompt("enter a number:"));
+    var number2= parseInt(prompt("enter another number:"));
+    var result= add(number1, number2);
+    alert(result);
+    //alert(add(number1, number2));
+    //SUBTRACTION FUNCTION
+    var subtract = function(number1, number2) {
+        return number1 - number2;
+        };
+        var number1= parseInt(prompt("enter a number:"));
+        var number2= parseInt(prompt("enter another number:"));
+        var result= subtract(number1, number2);
+        alert(result);
+
+        //MULTYPLICATION FUNCTION
+        var multiply = function(number1, number2) {
+            return number1 * number2;
+            };
+            var number1= parseInt(prompt("enter a number:"));
+            var number2= parseInt(prompt("enter another number:"));
+            var result= multiply(number1, number2);
+            alert(result);
+
+            //DIVISION FUNCTION
+            var divide = function(number1, number2) {
+                return number1 / number2;
+                };
+                var number1= parseInt(prompt("enter a number:"));
+                var number2= parseInt(prompt("enter another number:"));
+                var result= divide(number1, number2);
+                alert(result);
