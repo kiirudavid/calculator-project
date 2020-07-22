1cc344f04ef53bb8bf07f1204417550fa76e2b35
davidmaina085@gmail.com <davidmaina085@gmail.com>
2020-07-22 21:00:47 +0300

    separated business logic with UI

--- js/scripts.js
+++ js/scripts.js
@@ -1,34 +1,22 @@
-var add = function(number1, number2) {
-    return number1 + number2;
-    };
-    var number1= parseInt(prompt("enter a number:"));
-    var number2= parseInt(prompt("enter another number:"));
-    var result= add(number1, number2);
-    alert(result);
-    //alert(add(number1, number2));
-    //SUBTRACTION FUNCTION
-    var subtract = function(number1, number2) {
-        return number1 - number2;
-        };
-        var number1= parseInt(prompt("enter a number:"));
-        var number2= parseInt(prompt("enter another number:"));
-        var result= subtract(number1, number2);
-        alert(result);
-
-        //MULTYPLICATION FUNCTION
-        var multiply = function(number1, number2) {
+          var add = function(number1, number2) {
+            return number1 + number2;
+            };
+            var subtract = function(number1, number2) {
+            return number1 - number2;
+            };
+            var multiply = function(number1, number2) {
             return number1 * number2;
             };
-            var number1= parseInt(prompt("enter a number:"));
-            var number2= parseInt(prompt("enter another number:"));
-            var result= multiply(number1, number2);
-            alert(result);
-
-            //DIVISION FUNCTION
             var divide = function(number1, number2) {
-                return number1 / number2;
-                };
-                var number1= parseInt(prompt("enter a number:"));
-                var number2= parseInt(prompt("enter another number:"));
-                var result= divide(number1, number2);
-                alert(result);
+            return number1 / number2;
+            };
+            
+            $(document).ready(function() {
+            $("form#add").submit(function(event) {
+            event.preventDefault();
+            var number1 = parseInt($("#add1").val());
+            var number2 = parseInt($("#add2").val());
+            var result = add(number1, number2);
+            $("#output").text(result);
+            });
+            });
